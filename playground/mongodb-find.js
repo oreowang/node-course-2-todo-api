621addb4f7aafe59da9986c0f9d55f1c85b9f106
oreowang <wang4780@gmail.com>
2017-03-20 17:02:06 +0800

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,35 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) =>{
+  if(err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('58cf92c3a5aa07cf898e1464')
+  // }).toArray().then((docs) =>{
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) =>{
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').count().then((count) =>{
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) =>{
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({
+    name: 'Ira'
+  }).toArray().then((docs) =>{
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) =>{
+    console.log('Unable to tetch todos', err);
+  });
+
+  //db.close();
+});
