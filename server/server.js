8037a022d8f97f74a9981fa9211fa8ace065c1d0
oreowang <wang4780@gmail.com>
2017-03-29 00:21:45 +0800

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -97,7 +97,6 @@ app.patch('/todos/:id', (req, res) =>{
   })
 });
 
-//POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
@@ -115,7 +114,6 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-// POST /users/login {email, password}
 app.post('/users/login', (req, res) =>{
   var body = _.pick(req.body, ['email', 'password']);
 
@@ -124,6 +122,14 @@ app.post('/users/login', (req, res) =>{
       res.header('x-auth', token).send(user);
     });
   }).catch((e) =>{
+    res.status(400).send();
+  });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) =>{
+  req.user.removeToken(req.token).then(() =>{
+    res.status(200).send();
+  }, () =>{
     res.status(400).send();
   });
 });
